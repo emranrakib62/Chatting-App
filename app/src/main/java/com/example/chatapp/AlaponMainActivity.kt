f713e0402561caf896d25539b5876e2c5e348c93
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-01 23:33:06 +0600

    updated

--- app/src/main/java/com/example/chatapp/AlaponMainActivity.kt
+++ app/src/main/java/com/example/chatapp/AlaponMainActivity.kt
@@ -3,10 +3,8 @@ package com.example.chatapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AlaponMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
