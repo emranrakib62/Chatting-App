9440ed23fb1d85a24fc14932c656a62ba5a63595
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-01 23:31:14 +0600

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 
     // Kotlin
     implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
-
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
 
 
