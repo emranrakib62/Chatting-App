325d2936b6717773aada5e5e8d7590a306e6fbcd
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-02 22:48:51 +0600

    updated

--- app/src/main/java/com/example/chatapp/SigninFragment.kt
+++ app/src/main/java/com/example/chatapp/SigninFragment.kt
@@ -5,24 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.navigation.fragment.findNavController
+import com.example.chatapp.databinding.FragmentSigninBinding
+import kotlin.math.sign
 
 
 class SigninFragment : Fragment() {
 
-
+lateinit var binding: FragmentSigninBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding= FragmentSigninBinding.inflate(layoutInflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+        return binding.root
+    binding.btnRegister.setOnClickListener {
+
+        findNavController().navigate(R.id.action_signinFragment_to_registerFragment)
     }
+
+        binding.btnSignIn.setOnClickListener {
+            var email=binding.editTextEmail.text.toString()
+            var password =binding.editTextPassword.text.toString()
+            signInUser(email,password)
+        }
+
+    }
+
+    }
+
+private fun SigninFragment.signInUser(email: String, password: String) {
+
+
 
 
 }
+
+
