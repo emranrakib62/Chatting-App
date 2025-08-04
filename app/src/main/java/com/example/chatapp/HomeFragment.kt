82acc5220c87d09788b5d71a3076b60ecf537eb8
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-04 08:31:34 +0600

    updated

--- app/src/main/java/com/example/chatapp/HomeFragment.kt
+++ app/src/main/java/com/example/chatapp/HomeFragment.kt
@@ -5,20 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.chatapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class HomeFragment : Fragment() {
-
+lateinit var binding: FragmentHomeBinding
+lateinit var mAuth: FirebaseAuth
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        binding= FragmentHomeBinding.inflate(layoutInflater,container,false)
+
         return inflater.inflate(R.layout.fragment_home, container, false)
+    mAuth= FirebaseAuth.getInstance()
+
+        binding.btnLogout.setOnClickListener {
+            mAuth.signOut()
+            findNavController().navigate(R.id.action_homeFragment_to_signinFragment)
+        }
+        return binding.root
     }
 
 
-            }
+}
 
