5ad518fcec04afee87fda9115ae4f60687dc4c3d
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-05 15:37:15 +0600

    updated

---
+++ app/src/main/java/com/example/chatapp/User.kt
@@ -0,0 +1,14 @@
+package com.example.chatapp
+
+import android.R
+import android.location.Address
+
+data class User(
+var address: String,
+var email: String,
+var name: String,
+var password: String,
+var phone: String,
+)
+
+
---
+++ app/src/main/java/com/example/chatapp/UserAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.chatapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatapp.databinding.ItemUserFrBinding
+
+class UserAdapter: ListAdapter<User, UserAdapter.UserViewHolder>(comparator)
+{
+
+    class  UserViewHolder(var binding: ItemUserFrBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UserViewHolder {
+        return UserViewHolder(ItemUserFrBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(
+        holder: UserViewHolder,
+        position: Int
+    ) {
+        getItem(position).let {
+            holder.binding.nametv.text=it.name
+            holder.binding.mobiletv.text=it.phone
+        }
+    }
+companion object{
+
+
+    var comparator = object :DiffUtil.ItemCallback<User>(){
+        override fun areItemsTheSame(
+            oldItem: User,
+            newItem: User
+        ): Boolean {
+            return oldItem.email==newItem.email
+        }
+
+        override fun areContentsTheSame(
+            oldItem: User,
+            newItem: User
+        ): Boolean {
+           return oldItem==newItem
+        }
+
+    }
+}
+
+
+
+}
