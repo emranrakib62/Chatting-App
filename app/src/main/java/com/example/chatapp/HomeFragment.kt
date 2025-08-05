df6e18a85bbfceb5d9fba4858d71d3520bdb14ba
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-05 18:40:44 +0600

    updated

--- app/src/main/java/com/example/chatapp/HomeFragment.kt
+++ app/src/main/java/com/example/chatapp/HomeFragment.kt
@@ -8,12 +8,17 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.chatapp.databinding.FragmentHomeBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class HomeFragment : Fragment() {
 lateinit var binding: FragmentHomeBinding
 lateinit var mAuth: FirebaseAuth
-
+var userlist=mutableListOf<User>()
+lateinit var  adapter: UserAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,12 +28,36 @@ lateinit var mAuth: FirebaseAuth
 
 
         mAuth= FirebaseAuth.getInstance()
-
-        binding.btnLogout.setOnClickListener {
+adapter= UserAdapter()
+        binding.logoutBtn.setOnClickListener {
             mAuth.signOut()
             findNavController().navigate(R.id.action_homeFragment_to_signinFragment)
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FirebaseDatabase.getInstance().reference.child("user").addValueEventListener(object  : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                snapshot.children.forEach {
+                    var user:User = it.getValue(User::class.java)!!
+userlist.add(user)
+
+
+                }
+adapter.submitList(userlist)
+              binding.userRecycler.adapter=adapter
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+
     }
 
 
