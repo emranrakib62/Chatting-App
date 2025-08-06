94cdbe9647a445a8d849612e1e89e0f4d3d1c81d
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-06 22:35:18 +0600

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,5 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.2.3")
     implementation("androidx.fragment:fragment-ktx:1.3.3")
     implementation("com.github.dhaval2404:imagepicker:2.1")
+    implementation("com.karumi:dexter:6.2.3")
 }
--- app/src/main/java/com/example/chatapp/ProfileFragment.kt
+++ app/src/main/java/com/example/chatapp/ProfileFragment.kt
@@ -1,11 +1,20 @@
 package com.example.chatapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.chatapp.databinding.FragmentProfileBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.DexterError
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.PermissionRequestErrorListener
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.util.jar.Manifest
 
 
 class ProfileFragment : Fragment() {
@@ -27,4 +36,47 @@ class ProfileFragment : Fragment() {
 
         return binding.root
     }
+
+    private fun requestPermissions() {
+        // below line is use to request permission in the current activity.
+        // this method is use to handle error in runtime permissions
+        Dexter.withActivity(this) // below line is use to request the number of permissions which are required in our app.
+            .withPermissions(
+                Manifest.permission.CAMERA,  // below is the list of permissions
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_CONTACTS
+            ) // after adding permissions we are calling an with listener method.
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
+                    // this method is called when all permissions are granted
+                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                        // do you work now
+                        Toast.makeText(
+                            this@MainActivity,
+                            "All the permissions are granted..",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    // check for permanent denial of any permission
+                    if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
+                        // permission is denied permanently, we will show user a dialog message.
+                        showSettingsDialog()
+                    }
+                }
+
+                public override fun onPermissionRationaleShouldBeShown(
+                    list: MutableList<PermissionRequest?>?,
+                    permissionToken: PermissionToken
+                ) {
+                    // this method is called when user grants some permission and denies some of them.
+                    permissionToken.continuePermissionRequest()
+                }
+            }).withErrorListener(PermissionRequestErrorListener { error: DexterError? ->
+                // we are displaying a toast message for error message.
+                Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT)
+                    .show()
+            }) // below line is use to run the permissions on same thread and to check the permissions
+            .onSameThread().check()
+    }
+
 }
