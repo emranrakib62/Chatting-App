76c950aed9c3ac138cf2acb7cc0cca8705d00f4d
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-09 23:31:10 +0600

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,5 @@ dependencies {
     implementation("com.github.dhaval2404:imagepicker:2.1")
     implementation("com.karumi:dexter:6.2.3")
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
 }
